6dd6403a6c4f0f5f69e157bec32890cedea455e0
Talha Rao <60220164+Talha-Rao@users.noreply.github.com>
2021-08-20 16:02:41 +0500

    Update sonarqube-scanner.js

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,7 +1,7 @@
 const scanner = require('sonarqube-scanner');
 scanner(
     {
-        serverUrl: " https://82a9a09c039c.ngrok.io",
+        serverUrl: "https://9a4715a9dadd.ngrok.io",
         token: "90b039427b8023003b8eead81ad2f73bb0a15c2b",
         options: {
             "sonar.sources": "./src"
