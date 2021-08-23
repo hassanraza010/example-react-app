9af990f0049db4d4664481493642c9505aad9ecd
hassanraza010 <88310512+hassanraza010@users.noreply.github.com>
2021-08-23 20:27:01 +0500

    Update sonarqube-scanner.js

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -2,7 +2,7 @@ const scanner = require('sonarqube-scanner');
 scanner(
     {
         serverUrl: "http://localhost:9000",
-        token: "c508c07a8a7abc3d0a3d327832087573c411c7fe",
+        token: "2a81d074ef1701dbc78e75e30576daac30e46a1a",
         options: {
             "sonar.sources": "./src"
         },
