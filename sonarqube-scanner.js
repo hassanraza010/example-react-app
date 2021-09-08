db66304b4656d168198c5c5e1eddcd87d838c9f2
hassanraza010 <88310512+hassanraza010@users.noreply.github.com>
2021-09-08 18:57:31 +0500

    Update sonarqube-scanner.js

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,11 +1,1 @@
-const scanner = require('sonarqube-scanner');
-scanner(
-    {
-        serverUrl: "http://10.0.3.206:9000",
-        token: "2a81d074ef1701dbc78e75e30576daac30e46a1a",
-        options: {
-            "sonar.sources": "./src"
-        },
-    },
-    () => process.exit()
-)
+sonar.projectKey=hassanraza010_example-react-app
